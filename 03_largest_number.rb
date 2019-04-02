58a49511b69e6a8c1b5386b036342d7562eff8c9
louislloyd-besson <louislb2@gmail.com>
2019-04-03 09:07:27 +1000

    finished 03_largest_number.rb

--- 03_largest_number.rb
+++ 03_largest_number.rb
@@ -7,17 +7,27 @@
 #
 # Check your solution by filling in the folowing table:
 # number_1 || number_2 || expected || actual
-# 
+#
 # If your stuck try working togeather
-# 
+#
 # Beast Mode:
 # If you complete the challenge, feel free to come up
 # with your own examples and test corner cases.
-# 
+#
 # Examples - what if both numbers are equal or are strings?
 
-def largest_number (number_1, number_2)
-  # Your code here
+def largest_number(number_1, number_2)
+  if (number_1.is_a? Numeric) && (number_2.is_a? Numeric)
+    return number_1 >= number_2 ? number_1 : number_2
+  else
+    return "One or both supplied values is not a numeric value, and is therefore unsuitable for comparison."
+  end
 end
 
-largest_number(100,50)
+#   number_1  || number_2 || expected  || actual
+#     100     ||   50     ||    100    ||  100
+#     10      ||   20     ||    20     ||  20
+#     30      ||   30     ||    30     ||  30
+#     "ok"    ||  "cool"  ||    error  || "ok"  ## Occurred because at this point the function only contained 'return number_1 >= number_2 ? number_1 : number_2'. Both values got coerced to 0 for the comparison so whichever value was supplied first is returned.
+
+puts largest_number("ok", "cool")
